9a812fee86c8877b0659f3c176964560c5a5e272
Sandeep Ruttala <itssandeepruttala@gmail.com>
2024-10-21 10:42:31 +0530

    user schema completed

---
+++ server/Model/UserSchema.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema(
+    {
+        username:{
+            type: String,
+            required: true
+        },
+
+        email:{
+            type: String,
+            unique: true,
+            required: true
+        },
+
+        age:{
+            type: Number,
+            required: true,
+            min: 0
+        },
+
+        created_at:{
+            type: Date,
+            default: Date.now
+        }
+    }
+)
+
+const User = mongoose.model("User", UserSchema)
+
+module.exports = user;
