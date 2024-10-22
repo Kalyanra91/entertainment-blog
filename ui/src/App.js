b316a59822760578bc61d2845406221f7aad4078
Jayanth1312 <jayanthpaldugu3@gmail.com>
2024-10-22 22:31:04 +0530

    added register page

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
-    <>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Register />} />{" "}
+      </Routes>
+    </Router>
   );
 }
 
