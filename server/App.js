5ee0ed726f1d98bdf40a8b604879209866e43075
Sandeep Ruttala <itssandeepruttala@gmail.com>
2024-10-23 10:24:43 +0530

    added /users

--- server/App.js
+++ server/App.js
@@ -23,6 +23,15 @@ app.use(AuthController)
 const UserController = require('./Controller/UserController')
 app.use(UserController)
 
+// get users
+const User = require('./Model/UserSchema')
+app.get('/users', (req, res) => {
+    User.find().then((response) => {
+        res.status(200).json(response)
+    }).catch((e) => {
+        res.status(500).json({ message: e })
+    })
+})
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
