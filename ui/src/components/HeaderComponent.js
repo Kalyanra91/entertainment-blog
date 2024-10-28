e4914d66c160bc16a633ebba9f08d075208cd761
Jayanth1312 <jayanthpaldugu3@gmail.com>
2024-10-28 11:16:38 +0530

    modified username header

--- ui/src/components/HeaderComponent.js
+++ ui/src/components/HeaderComponent.js
@@ -1,13 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import CustomInput from "../components/CustomInput";
 import logo from "../assets/logo.png";
 import "../styles/header.css";
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(";").shift();
+}
 
 const Header = () => {
   const [searchValue, setSearchValue] = useState("");
   const [isCategoryOpen, setIsCategoryOpen] = useState(false);
   const [isProfileOpen, setIsProfileOpen] = useState(false);
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [profile, setProfile] = useState({ username: "User" });
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    const username = getCookie("username");
+
+    fetch(`http://localhost:3001/profile/${username}`, {
+      method: "GET",
+      credentials: "include",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const date = new Date(data.created_at);
+        const options = { year: "numeric", month: "long" };
+        data.created_at = date.toLocaleDateString("en-US", options);
+        setProfile(data);
+      })
+      .catch((error) => {
+        navigate("/login");
+        console.error("Error:", error);
+      });
+  }, []);
 
   const toggleMobileMenu = (e) => {
     e.stopPropagation();
@@ -51,6 +80,7 @@ const Header = () => {
     })
       .then((response) => {
         if (response.ok) {
+          localStorage.removeItem("username");
           window.location.href = "/login";
         }
       })
@@ -99,10 +129,10 @@ const Header = () => {
                         viewBox="0 0 24 24"
                         fill="none"
                         stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        class="feather feather-film me-2"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="feather feather-film me-2"
                       >
                         <rect
                           x="2"
@@ -130,10 +160,10 @@ const Header = () => {
                         viewBox="0 0 24 24"
                         fill="none"
                         stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        class="feather feather-tv me-2"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="feather feather-tv me-2"
                       >
                         <rect
                           x="2"
@@ -155,10 +185,10 @@ const Header = () => {
                         viewBox="0 0 24 24"
                         fill="none"
                         stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        class="feather feather-music me-2"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="feather feather-music me-2"
                       >
                         <path d="M9 18V5l12-2v13"></path>
                         <circle cx="6" cy="18" r="3"></circle>
@@ -174,10 +204,10 @@ const Header = () => {
                         viewBox="0 0 24 24"
                         fill="none"
                         stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        class="feather feather-more-horizontal me-2"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="feather feather-more-horizontal me-2"
                       >
                         <circle cx="12" cy="12" r="1"></circle>
                         <circle cx="19" cy="12" r="1"></circle>
@@ -217,7 +247,7 @@ const Header = () => {
               <div className="profile-icon-container">
                 <i className="fa-solid fa-user profile-icon"></i>
               </div>
-              {/* <div className="profile-name">{ profile.username }</div> */}
+              <div className="profile-name">{profile.username}</div>
             </button>
 
             {isProfileOpen && (
@@ -230,10 +260,10 @@ const Header = () => {
                     viewBox="0 0 24 24"
                     fill="none"
                     stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    class="feather feather-user me-2"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-user me-2"
                   >
                     <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
                     <circle cx="12" cy="7" r="4"></circle>
@@ -248,10 +278,10 @@ const Header = () => {
                     viewBox="0 0 24 24"
                     fill="none"
                     stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    class="feather feather-log-out me-2"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-log-out me-2"
                   >
                     <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
                     <polyline points="16 17 21 12 16 7"></polyline>
